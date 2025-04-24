4dcd4891e9a6b7e29c82ef454fa90484816bb374
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2025-04-24 19:23:31 +0200

    add generator

---
+++ tools/generators/migration/README.md
@@ -0,0 +1,7 @@
+# migration
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build migration` to build the library.
---
+++ tools/generators/migration/src/index.ts
@@ -0,0 +1,20 @@
+import { formatFiles, generateFiles, Tree } from '@nx/devkit';
+import * as path from 'path';
+
+export default async function (tree: Tree, schema: any) {
+  const timestamp = new Date().toISOString().replace(/[-T:\.Z]/g, '').substring(0, 14);
+  const fileName = `${timestamp}_${schema.name}.sql`;
+
+  generateFiles(
+    tree,
+    path.join(__dirname, 'files'),
+    'supabase/migrations',
+    {
+      fileName,
+      name: schema.name,
+      template: ''
+    }
+  );
+
+  await formatFiles(tree);
+}
